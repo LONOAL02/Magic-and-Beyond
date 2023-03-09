627167bba2e4d5e3dc8a07fd440432cc8f155484
Gonzalo Arca Iglesias <garcaiglesias@danielcastelao.org>
2023-03-09 14:49:49 +0100

    reliquias1

--- src/main/java/com/proyecto/inventario/Reliquias.java
+++ src/main/java/com/proyecto/inventario/Reliquias.java
@@ -1,4 +1,9 @@
 package com.proyecto.inventario;
 
 public class Reliquias {
+    public Item monedaAntigua = new Item("Moneda antigua.","Ganas un +20% de oro extra cada vez que ganas oro .",1,Item.RELIQUIA);
+    public Item corazónDorado = new Item("Corazón dorado.","Obtienes 75 de vida máxima .",1,Item.RELIQUIA);
+    public Item bendicionDeLaDiosa = new Item("Bendición De La Diosa.","Reduce a la mitad el coste de mana de tus hechizos .",1,Item.RELIQUIA);
+    public Item  aguaBendita= new Item("Agua Bendita.","Te cura un 10% de vida por turno .",1,Item.RELIQUIA);
+
 }
